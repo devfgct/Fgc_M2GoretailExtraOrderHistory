51d2110f2f60f96b62c7023283d35d3fc5f1d38c
KhoaZero123 <khoazero123@gmail.com>
2018-05-19 18:09:00 +0700

    Track create shipment

--- app/code/Goretail/ExtraOrderHistory/Observer/AddComment.php
+++ app/code/Goretail/ExtraOrderHistory/Observer/AddComment.php
@@ -2,45 +2,37 @@
 namespace Goretail\ExtraOrderHistory\Observer;
 use Magento\Framework\Event\ObserverInterface;
 
-class SalesOrderAfterSave implements ObserverInterface {
+class AddComment implements ObserverInterface {
 	protected $_coreRegistry;
 	protected $_authSession;
 
 	public function __construct(
 		\Magento\Framework\Registry $coreRegistry,
 		\Magento\Backend\Model\Auth\Session $authSession,
-		\Magento\Customer\Model\Session $customerSession
+		\Magento\Customer\Model\Session $customerSession,
+		\Magento\Framework\App\RequestInterface $request
 	) {
 		$this->_coreRegistry = $coreRegistry;
 		$this->_authSession = $authSession;
 		$this->_customerSession = $customerSession;
+		$this->_request = $request;
 	}
     public function execute(\Magento\Framework\Event\Observer $observer) {
 		$objectManager = \Magento\Framework\App\ObjectManager::getInstance();
 		$event = $observer->getEvent();
-		$customer = $event->getCustomer();
-		$eventName = $event->getName();
-		$eventData = $event->getData();
+		//$customer = $event->getCustomer();
+		//$eventName = $event->getName();
+		//$eventData = $event->getData();
 		$order = $event->getOrder();
+		$invoice = $observer->getEvent()->getInvoice();
 		if ($order instanceof \Magento\Framework\Model\AbstractModel) {
 			$origData = $order->getOrigData();
 			$data = $order->getData();
-			/**
-			 * Submit Invoice
-			 * $origData: ['state' => 'new', 'status' => 'pending']
-			 * $data: ['state' => 'processing', 'status' => 'processing']
-			 *
-			 * Hold
-			 * $data: ['state' => 'holded', 'status' => 'holded']
-			 *
-			 * Unhold
-			 * $data: ['state' => 'processing', 'status' => 'processing']
-			 */
 			if(!$origData || ($origData['state'] != $data['state'])) {
 				if($order->getCustomerIsGuest()) {
 					$by = $order->getCustomerEmail(); // 'Guest';
 				} elseif($this->_customerSession->isLoggedIn()) {
-					$by = $this->_customerSession->getCustomer()->getName();
+					$by = 'Customer'; //$this->_customerSession->getCustomer()->getName();
 				} else {
 					$by = $this->_authSession->getUser()->getUsername();
 				}
@@ -60,15 +52,22 @@ class SalesOrderAfterSave implements ObserverInterface {
 					);
 				}
 
-				if($order->getEntityId()) {
-					$comment = $order->addStatusHistoryComment($message)->setIsCustomerNotified(false)->setEntityName('order');
-					if($order->getState() == 'new') {
-						$comment->setIsVisibleOnFront(true);
-					}
-					$comment->save();
-				}
-				return $this;
+
 			}
+		} elseif (($invoice instanceof \Magento\Framework\Model\AbstractModel) && ($orderId = $this->_request->getParam('order_id'))) {
+			$order = $objectManager->create('\Magento\Sales\Model\Order')->load($orderId);
+			$message = __(
+				'Order #%1 has created invoice by %2.',
+				$order->getIncrementId(),
+				$this->_authSession->getUser()->getUsername()
+			);
+		}
+		if($order && $order->getEntityId() && isset($message)) {
+			$comment = $order->addStatusHistoryComment($message)->setIsCustomerNotified(false)->setEntityName('order');
+			if($order->getState() == 'new') {
+				$comment->setIsVisibleOnFront(true);
+			}
+			$comment->save();
 		}
 		return $this;
 	}
